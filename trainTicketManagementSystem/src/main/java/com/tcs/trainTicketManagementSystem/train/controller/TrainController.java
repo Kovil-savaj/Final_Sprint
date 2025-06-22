27b414fd66027704eb9712ccc2187fef67785077
Harshit Khandelwal <86138193+Harshit2756@users.noreply.github.com>
2025-06-22 22:24:32 +0530

    Refactor TrainController and UserController to improve code organization and add CrossOrigin support

--- trainTicketManagementSystem/src/main/java/com/tcs/trainTicketManagementSystem/train/controller/TrainController.java
+++ trainTicketManagementSystem/src/main/java/com/tcs/trainTicketManagementSystem/train/controller/TrainController.java
@@ -1,12 +1,31 @@
 package com.tcs.trainTicketManagementSystem.train.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.tcs.trainTicketManagementSystem.train.dto.TrainRequest;
 import com.tcs.trainTicketManagementSystem.train.dto.TrainResponse;
 import com.tcs.trainTicketManagementSystem.train.dto.TrainSearchRequest;
-import com.tcs.trainTicketManagementSystem.train.exception.TrainAlreadyExistsException;
-import com.tcs.trainTicketManagementSystem.train.exception.TrainNotFoundException;
 import com.tcs.trainTicketManagementSystem.train.model.TrainStatus;
 import com.tcs.trainTicketManagementSystem.train.service.TrainService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,16 +34,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.List;
 
 /**
  * REST Controller for Train operations.
@@ -33,6 +42,8 @@ import java.util.List;
 @RequestMapping("/api/v1/trains")
 @Validated
 @Tag(name = "Train Management", description = "APIs for managing trains, schedules, and fare types")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public class TrainController {
 
     private static final Logger logger = LoggerFactory.getLogger(TrainController.class);
